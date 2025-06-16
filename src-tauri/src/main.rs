7a0116c7be79fc4132507f5e890f51d347ad5810
kwguo <shana0440@gmail.com>
2025-06-16 18:17:06 +0800

    separate torrent and ui

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod torrent;
-
 #[tokio::main]
 async fn main() {
     bitdrift_lib::run()
--- torrent/src/metainfo.rs
+++ torrent/src/metainfo.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
-// https://bittorrent.org/beps/bep_0003.html#metainfo-files
+// implementation of https://bittorrent.org/beps/bep_0003.html#metainfo-files
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MetaInfo {
     pub announce: String,
